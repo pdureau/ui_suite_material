a29d3c12e12467a1275e1f80115dc41269b29ca7
Pierre <pierredureau@yahoo.fr>
2022-09-22 10:54:57 +0200

    Fix top_app_bar; Add icon_button

--- templates/patterns/top_app_bar/top_app_bar_init.js
+++ templates/patterns/top_app_bar/top_app_bar_init.js
@@ -1,6 +1,6 @@
 (function () {
   "use strict";
-  const bars = document.querySelectorAll(".mdc-app-bar");
+  const bars = document.querySelectorAll(".mdc-top-app-bar");
   for (const bar of bars) {
     mdc.topAppBar.MDCTopAppBar.attachTo(bar);
   }
